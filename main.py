4770209d5385c972a433fe28651b6b732cb746e7
Luciano <lucianocanton18@gmail.com>
2021-03-24 12:29:30 -0300

    Added Flasks POST request and response

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 from google.cloud import vision
-import io
+from flask import Flask, jsonify, request
+import os
 
-path = "/home/luciano/Documents/chBendDev/nf.JPG"
-def detect_text(path):
-    """Detects text in the file."""
-    client = vision.ImageAnnotatorClient()
+app = Flask(__name__)
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
 
-    image = vision.Image(content=content)
+@app.route('/text_detector', methods=['POST'])
+def detect_text():
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
+    data = request.get_json()
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
-
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+    response = {
+        'message' : 'thank you',
+    }
+    
+    return jsonify(response), 200
+    
+    
 
 if __name__ == '__main__':
-    detect_text(path)
+    if os.environ.get('GAE_ENV') != 'standard':
+        app.run(host='127.0.0.1', port=8080, debug=True)
